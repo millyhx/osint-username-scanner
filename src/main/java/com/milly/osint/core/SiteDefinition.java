331e23c6be276e1728c93d2f78a3641058f83181
millyhx <millycyb@outlook.com>
2026-05-30 11:35:01 +0100

    Set up SiteDefinition this represents one website we'll check.

--- src/main/java/com/milly/osint/core/SiteDefinition.java
+++ src/main/java/com/milly/osint/core/SiteDefinition.java
@@ -1,4 +1,21 @@
 package com.milly.osint.core;
 
 public class SiteDefinition {
+
+    private String name;
+    private String url;
+    private int existsWhenStatus;
+
+    public String getName() {
+        return name;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public int getExistsWhenStatus() {
+        return existsWhenStatus;
+    }
+
 }
