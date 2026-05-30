7d1a32f6dc91f10aa456c52a996ed8d588ad9149
millyhx <millycyb@outlook.com>
2026-05-30 11:44:47 +0100

    Logic for a single shared HTTP Client.

--- src/main/java/com/milly/osint/http/HttpClientProvider.java
+++ src/main/java/com/milly/osint/http/HttpClientProvider.java
@@ -1,4 +1,12 @@
 package com.milly.osint.http;
 
+import java.net.http.HttpClient;
+
 public class HttpClientProvider {
+
+    private static final HttpClient client = HttpClient.newHttpClient();
+
+    public static HttpClient get() {
+        return client;
+    }
 }
