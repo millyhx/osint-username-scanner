6170c70585685fc25a956f1a38beea10cd5e2344
millyhx <millycyb@outlook.com>
2026-05-30 14:15:00 +0100

    Creating the scene using fxml, updating dependencies

--- src/main/java/com/milly/osint/App.java
+++ src/main/java/com/milly/osint/App.java
@@ -1,18 +1,23 @@
 package com.milly.osint;
 
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 public class App extends Application {
 
     @Override
-    public void start(Stage stage) {
-        // TODO: Load JavaFx UI
+    public void start(Stage stage) throws Exception {
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("/ui/main.fxml"));
+        Scene scene = new Scene(loader.load(), 800, 600);
+
         stage.setTitle("OSINT Username Scanner");
+        stage.setScene(scene);
         stage.show();
     }
 
-    public static void main(String[] args){
+    public static void main(String[] args) {
         launch();
     }
 }
--- src/main/java/com/milly/osint/ui/MainController.java
+++ src/main/java/com/milly/osint/ui/MainController.java
@@ -1,4 +1,21 @@
 package com.milly.osint.ui;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
+
 public class MainController {
+
+    @FXML private TextField usernameField;
+    @FXML private TableView<?> resultsTable;
+    @FXML private TableColumn<?, ?> siteColumn;
+    @FXML private TableColumn<?, ?> existsColumn;
+    @FXML private TableColumn<?, ?> urlColumn;
+
+    @FXML
+    private void onScanClicked() {
+        System.out.println("Scan clicked! Username: " + usernameField.getText());
+    }
 }
